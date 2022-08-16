17999ab2f8f42636c0b9474e28c8af3e95f81a8e
SheeTabz <wmuriithitabitha@gmail.com>
2022-08-16 11:06:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if(ride<="400"){
+    return 'This one is on me!'
+  }
+  else if(ride >2000 && ride <=2500)
+  {
+    
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if(ride > 2500) {
+    return 'No can do.'
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const result = city === "NYC"? 'Ok, sounds good.' : 'No go.'
+  return result
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous': return 'Thank you so much.'
+    break;
+    case 'not as generous': return 'Thank you.'
+    break;
+    default: return 'Bye.';
+  }
 }
